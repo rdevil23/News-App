e6740e27c19f9dc5c326b03ae86a8d8ee48bb196
rdevil23 <rdevil1337@gmail.com>
2023-12-10 19:00:33 +0500

    feat: add main block

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/Header/Header';
+import Main from './pages/Main';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="container">
+        <Main />
+      </div>
     </>
   );
 }
---
+++ src/pages/Main.jsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+
+const Main = () => {
+  return <main className={styles.main}>Main</main>;
+};
+
+export default Main;
