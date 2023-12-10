028badcaeb34fe0c96d34a9b1c9df9bb00f56ec6
rdevil23 <rdevil1337@gmail.com>
2023-12-10 19:38:37 +0500

    feat: add helper time function

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -1,11 +1,13 @@
-import { formatDate } from '../../helpers/formatDate';
+import { formatTimeAgo } from '../../helpers/formatTimeAgo';
 import styles from './styles.module.css';
 
 const NewsBanner = ({ item }) => {
   return (
     <div className={styles.header}>
       <h3 className={styles.title}>{item.title}</h3>
-      <p className={styles.date}>{formatDate(new Date())}</p>
+      <p className={styles.date}>
+        {formatTimeAgo(item.published)} by {item.author}
+      </p>
     </div>
   );
 };
