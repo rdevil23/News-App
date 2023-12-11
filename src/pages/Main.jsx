011d65a18b5c7c859f81d01659fd15af1b78b8d6
rdevil23 <rdevil1337@gmail.com>
2023-12-11 20:57:30 +0500

    feat: add news list

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import NewsBanner from '../components/NewsBanner/NewsBanner';
 import styles from './styles.module.css';
 import { getNews } from '../api/apiNews';
+import NewsList from '../components/NewsList/NewsList';
 
 const Main = () => {
   const [news, setNews] = useState([]);
@@ -18,8 +19,12 @@ const Main = () => {
     };
     fetchNews();
   }, []);
+
   return (
-    <main className={styles.main}>{news.length > 0 ? <NewsBanner item={news[0]} /> : null}</main>
+    <main className={styles.main}>
+      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+      <NewsList news={news} />
+    </main>
   );
 };
 
