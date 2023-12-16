e655cfc945a0ca30bc961c30f28c171cdf99665c
rdevil23 <rdevil1337@gmail.com>
2023-12-16 19:15:52 +0500

    refactoring

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -3,6 +3,12 @@ import styles from './styles.module.css';
 const Categories = ({ categories, selectedCategory, setSelectedCategory }) => {
   return (
     <div className={styles.categories}>
+      <button
+        onClick={() => setSelectedCategory(null)}
+        className={!selectedCategory ? styles.active : styles.item}
+      >
+        All
+      </button>
       {categories.map((category) => {
         return (
           <button
